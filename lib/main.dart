173ad74b6f15333eb96defa3796a2cce509afb39
Mansi <mansi.jain@vistaprint.com>
2020-11-17 23:43:18 -0500

    inital login screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,131 +4,287 @@ import 'package:carousel_pro/carousel_pro.dart';
 import 'package:ohack/horizontal.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ));
+  runApp(MyApp());
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Widget image_carousel = new Container(
-      height: 300.0,
-      child: new Carousel(
-        boxFit: BoxFit.cover,
-        images: [
-          AssetImage('images/c1.jpg'),
-          AssetImage('images/c1.jpg'),
-          AssetImage('images/c1.jpg'),
-          AssetImage('images/c1.jpg'),
-          AssetImage('images/c1.jpg'),
-        ],
-        autoplay: false,
-        animationCurve: Curves.fastOutSlowIn,
-        animationDuration: Duration(milliseconds: 1000),
+    return MaterialApp(
+      title: 'Thoorgai',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Thoorgayi Login'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final emailField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.blue,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FirstRoute()),
+          );
+        },
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
       ),
     );
 
     return Scaffold(
-      appBar: new AppBar(
-        //centerTitle: true,
-        title: Text('Payir'),
-        actions: [
-          new IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.white,
-              ),
-              onPressed: () {}),
-          new IconButton(
-              icon: Icon(
-                Icons.shopping_cart,
-                color: Colors.white,
-              ),
-              onPressed: () {})
-        ],
-      ),
-      drawer: new Drawer(
-        child: new ListView(
-          children: [
-            new UserAccountsDrawerHeader(
-              accountName: Text('Vatsal'),
-              accountEmail: Text('vatsal.palan@somaiya.edu'),
-              currentAccountPicture: GestureDetector(
-                child: new CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  child: Icon(
-                    Icons.person,
-                    color: Colors.white,
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 155.0,
+                  child: Image.asset(
+                    "lib/assets/images/logo.png",
+                    fit: BoxFit.contain,
                   ),
                 ),
-              ),
+                SizedBox(height: 45.0),
+                emailField,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
             ),
-            InkWell(
-              onTap: () {},
-              child: ListTile(
-                title: Text('Home Page'),
-                leading: Icon(Icons.home),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: ListTile(
-                title: Text('My account'),
-                leading: Icon(Icons.person),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: ListTile(
-                title: Text('My orders'),
-                leading: Icon(Icons.shopping_basket),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: ListTile(
-                title: Text('Home Page'),
-                leading: Icon(Icons.dashboard),
-              ),
-            ),
-            Divider(),
-            InkWell(
-              onTap: () {},
-              child: ListTile(
-                title: Text('Settings'),
-                leading: Icon(Icons.settings),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: ListTile(
-                title: Text('About'),
-                leading: Icon(Icons.help),
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: new ListView(
-        children: [
-          image_carousel,
-          new Padding(
-            //padding the categories
-            padding: const EdgeInsets.all(8.0),
-            child: new Text('Categories'),
           ),
-
-          //horizontal items
-          Horizontal(),
-        ],
+        ),
       ),
     );
   }
 }
+
+class FirstRoute extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Payir - Thoorgayi'),
+      ),
+      // body: Center(
+      //   child: ElevatedButton(
+      //     child: Text('Open route'),
+      //     onPressed: () {
+      //       // Navigate to second route when tapped.
+      //     },
+      //   ),
+      // ),
+      body: new ListView(
+        children: [
+          ElevatedButton(onPressed: null, child: Text('Invoice')),
+          ElevatedButton(onPressed: null, child: Text('Sales')),
+          ElevatedButton(onPressed: null, child: Text('Purchase')),
+          ElevatedButton(onPressed: null, child: Text('Inventory')),
+          ElevatedButton(onPressed: null, child: Text('Report')),
+        ],
+      ),
+
+    );
+  }
+}
+
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+//
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget image_carousel = new Container(
+//       height: 300.0,
+//       child: new Carousel(
+//         boxFit: BoxFit.cover,
+//         images: [
+//           AssetImage('images/c1.jpg'),
+//           AssetImage('images/c1.jpg'),
+//           AssetImage('images/c1.jpg'),
+//           AssetImage('images/c1.jpg'),
+//           AssetImage('images/c1.jpg'),
+//         ],
+//         autoplay: false,
+//         animationCurve: Curves.fastOutSlowIn,
+//         animationDuration: Duration(milliseconds: 1000),
+//       ),
+//     );
+//
+//     return Scaffold(
+//       appBar: new AppBar(
+//         //centerTitle: true,
+//         title: Text('Payir'),
+//         actions: [
+//           new IconButton(
+//               icon: Icon(
+//                 Icons.search,
+//                 color: Colors.white,
+//               ),
+//               onPressed: () {}),
+//           new IconButton(
+//               icon: Icon(
+//                 Icons.shopping_cart,
+//                 color: Colors.white,
+//               ),
+//               onPressed: () {})
+//         ],
+//       ),
+//       drawer: new Drawer(
+//         child: new ListView(
+//           children: [
+//             new UserAccountsDrawerHeader(
+//               accountName: Text('Vatsal'),
+//               accountEmail: Text('vatsal.palan@somaiya.edu'),
+//               currentAccountPicture: GestureDetector(
+//                 child: new CircleAvatar(
+//                   backgroundColor: Colors.grey,
+//                   child: Icon(
+//                     Icons.person,
+//                     color: Colors.white,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             InkWell(
+//               onTap: () {},
+//               child: ListTile(
+//                 title: Text('Home Page'),
+//                 leading: Icon(Icons.home),
+//               ),
+//             ),
+//             InkWell(
+//               onTap: () {},
+//               child: ListTile(
+//                 title: Text('My account'),
+//                 leading: Icon(Icons.person),
+//               ),
+//             ),
+//             InkWell(
+//               onTap: () {},
+//               child: ListTile(
+//                 title: Text('My orders'),
+//                 leading: Icon(Icons.shopping_basket),
+//               ),
+//             ),
+//             InkWell(
+//               onTap: () {},
+//               child: ListTile(
+//                 title: Text('Home Page'),
+//                 leading: Icon(Icons.dashboard),
+//               ),
+//             ),
+//             Divider(),
+//             InkWell(
+//               onTap: () {},
+//               child: ListTile(
+//                 title: Text('Settings'),
+//                 leading: Icon(Icons.settings),
+//               ),
+//             ),
+//             InkWell(
+//               onTap: () {},
+//               child: ListTile(
+//                 title: Text('About'),
+//                 leading: Icon(Icons.help),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       body: new ListView(
+//         children: [
+//           image_carousel,
+//           new Padding(
+//             //padding the categories
+//             padding: const EdgeInsets.all(8.0),
+//             child: new Text('Categories'),
+//           ),
+//
+//           //horizontal items
+//           Horizontal(),
+//         ],
+//       ),
+//     );
+//   }
+// }
