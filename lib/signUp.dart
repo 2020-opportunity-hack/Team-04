7570a0352a86a2ebc0c7df8662922998db5096e7
vatsal1999 <vatsal.palan@somaiya.edu>
2020-11-19 09:05:13 +0530

    auth

--- lib/signUp.dart
+++ lib/signUp.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:ohack/main.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  final _auth = FirebaseAuth.instance;
+  bool showProgress = false;
+  String email, password;
+
   @override
   Widget build(BuildContext context) {
     final emailField = TextField(
       obscureText: false,
       style: style,
+      onChanged: (value) {
+        email = value;
+      },
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           hintText: "Email",
@@ -17,21 +31,24 @@ class SignUp extends StatelessWidget {
     final passwordField = TextField(
       obscureText: true,
       style: style,
+      onChanged: (value) {
+        password = value;
+      },
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           hintText: "Password",
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
-    final reEnterPasswordField = TextField(
-      obscureText: true,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Re-enter Password",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
+    // final reEnterPasswordField = TextField(
+    //   obscureText: true,
+    //   style: style,
+    //   decoration: InputDecoration(
+    //       contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+    //       hintText: "Re-enter Password",
+    //       border:
+    //           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    // );
 
     final loginButon = Material(
       elevation: 5.0,
@@ -43,7 +60,8 @@ class SignUp extends StatelessWidget {
         onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => MyHomePage()),
+            MaterialPageRoute(
+                builder: (context) => MyHomePage(title: 'Thoorgayi Login')),
           );
         },
         child: Text("Login",
@@ -60,12 +78,32 @@ class SignUp extends StatelessWidget {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MyHomePage()),
-          );
+
+        onPressed: () async {
+          setState(() {
+            showProgress = true;
+          });
+          try {
+            final newuser = await _auth.createUserWithEmailAndPassword(
+                email: email, password: password);
+            if (newuser != null) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyHomePage()),
+              );
+              setState(() {
+                showProgress = false;
+              });
+            }
+          } catch (e) {}
         },
+
+        // onPressed: () {
+        //   Navigator.push(
+        //     context,
+        //     MaterialPageRoute(builder: (context) => MyHomePage()),
+        //   );
+        // },
         child: Text("Sign Up",
             textAlign: TextAlign.center,
             style: style.copyWith(
@@ -94,8 +132,8 @@ class SignUp extends StatelessWidget {
                 emailField,
                 SizedBox(height: 25.0),
                 passwordField,
-                SizedBox(height: 25.0),
-                reEnterPasswordField,
+                // SizedBox(height: 25.0),
+                // reEnterPasswordField,
                 SizedBox(
                   height: 35.0,
                 ),
@@ -113,32 +151,32 @@ class SignUp extends StatelessWidget {
   }
 }
 
-class Category extends StatelessWidget {
-  final String image_location;
-  final String image_caption;
+// class Category extends StatelessWidget {
+//   final String image_location;
+//   final String image_caption;
 
-  Category({this.image_location, this.image_caption});
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(2.0),
-      child: InkWell(
-        onTap: () {},
-        child: Container(
-          width: 100.0,
-          child: ListTile(
-            title: Image.asset(
-              image_location,
-              width: 100.0,
-              height: 80.0,
-            ),
-            subtitle: Container(
-              alignment: Alignment.topCenter,
-              child: Text(image_caption),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   Category({this.image_location, this.image_caption});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(2.0),
+//       child: InkWell(
+//         onTap: () {},
+//         child: Container(
+//           width: 100.0,
+//           child: ListTile(
+//             title: Image.asset(
+//               image_location,
+//               width: 100.0,
+//               height: 80.0,
+//             ),
+//             subtitle: Container(
+//               alignment: Alignment.topCenter,
+//               child: Text(image_caption),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
