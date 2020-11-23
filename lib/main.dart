f36d84fd302989ae986548836ecd2a2bee551dff
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-11-22 21:49:57 -0500

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       supportedLocales: context.supportedLocales,
       localizationsDelegates: context.localizationDelegates,
       title: 'Thoorgai',
